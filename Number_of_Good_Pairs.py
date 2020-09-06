fbb7e38ae71bfbbb36d62eb0c745ebb605b6e15a
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-09-07 01:23:07 +0200

    update

--- Number_of_Good_Pairs.py
+++ Number_of_Good_Pairs.py
@@ -6,3 +6,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        mydict = Counter(nums)
+        print(mydict)
+        ssum = 0
+        for i in mydict.values():
+            if i==1:
+                continue
+            ssum = ssum + sum(range(i))
+        return ssum
