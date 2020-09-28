d8907597d5376f2f227e06963890441957231e7f
Aydin Ahmadli <35424859+buzzer4mornin@users.noreply.github.com>
2020-09-28 18:44:48 +0200

    new lc solution

--- Count_Negative_Numbers_in_a_Sorted_Matrix.py
+++ Count_Negative_Numbers_in_a_Sorted_Matrix.py
@@ -4,3 +4,13 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        result = 0
+        tot = 0
+        for list in grid:
+            for i in range(len(list)):
+                if list[i] >= 0:
+                    continue
+                tot = (len(list) - i)
+                break
+            result += tot
+        return result
